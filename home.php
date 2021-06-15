f0e2fa4a06a1f48640a20d9f6010a03e31f8daa2
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-06-15 19:25:43 +0200

    Classa Database

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php
+require 'Database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+
+<body>
+    <div>
+        <h1>Mon blog</h1>
+        <p>En construction</p>
+
+        <?php
+        $db = new Database();
+        echo $db->getConnection();
+        ?>
+    </div>
+</body>
+
+</html>
