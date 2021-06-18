f33a3fd9aec04af9191bb54a5e3743be23982654
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-06-18 14:18:11 +0200

    Ajouter un article

--- config/Router.php
+++ config/Router.php
@@ -2,17 +2,20 @@
 
 namespace App\config;
 
+use App\src\controller\BackController;
 use App\src\controller\FrontController;
 use App\src\controller\ErrorController;
 use Exception;
 
 class Router
 {
+    private $backController;
     private $frontController;
     private $errorController;
 
     public function __construct()
     {
+        $this->backController = new BackController;
         $this->frontController = new FrontController;
         $this->errorController = new ErrorController;
     }
@@ -23,10 +26,12 @@ class Router
             if (isset($_GET['route'])) {
                 if ($_GET['route'] === 'article') {
                     $this->frontController->article($_GET['articleId']);
+                } elseif ($_GET['route'] === 'addArticle') {
+                    $this->backController->addArticle($_POST);
                 } else {
                     $this->errorController->errorNotFound();
                 }
-            } else {
+            } else {*
                 $this->frontController->home();
             }
         } catch (Exception $e) {
