f4aeba5ba9847efebdc94762dc7570a96fdde9ef
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-06-18 13:41:27 +0200

    View

--- templates/home.php
+++ templates/home.php
@@ -1,35 +1,17 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-
-<body>
-    <div>
-        <h1>Mon blog</h1>
-        <p>En construction</p>
+<?php $this->title = 'Accueil' ?>
 
 
-        <?php
-        foreach ($articles as $article) {
-        ?>
+<?php
+foreach ($articles as $article) {
+?>
 
-        <div>
-            <h2><a href="../public/index.php?route=article&articleId=<?php echo htmlspecialchars($article->getId()) ?>"><?php echo htmlspecialchars($article->getTitle()); ?></a></h2>
-            <p><?php echo htmlspecialchars($article->getContent()); ?></p>
-            <p><?php echo htmlspecialchars($article->getAuthor()); ?></p>
-            <p>Créé le : <?php echo htmlspecialchars($article->getCreatedAt()); ?></p>
-        </div>
-        <br>
+<div>
+    <h2><a href="../public/index.php?route=article&articleId=<?php echo htmlspecialchars($article->getId()) ?>"><?php echo htmlspecialchars($article->getTitle()); ?></a></h2>
+    <p><?php echo htmlspecialchars($article->getContent()); ?></p>
+    <p><?php echo htmlspecialchars($article->getAuthor()); ?></p>
+    <p>Créé le : <?php echo htmlspecialchars($article->getCreatedAt()); ?></p>
+</div>
+<br>
 
-        <?php
-        }
-        ?>
-    </div>
-</body>
-
-</html>
+<?php
+}
--- templates/single.php
+++ templates/single.php
@@ -1,50 +1,27 @@
 <?php
-
-use App\src\DAO\ArticleDAO;
-use App\src\DAO\CommentDAO;
-
+$this->title = 'Article';
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
+<div>
+    <h2><?= htmlspecialchars($article->getTitle()); ?></h2>
+    <p><?= htmlspecialchars($article->getContent()); ?></p>
+    <p><?= htmlspecialchars($article->getAuthor()); ?></p>
+    <p>Créé le : <?= htmlspecialchars($article->getCreatedAt()); ?></p>
+</div>
+<br>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<a href="../public/index.php">Retour à l'accueil</a>
 
-<body>
-    <h1>Mon blog</h1>
+<div>
+    <h3>Commentaires</h3>
 
     <?php
-
+    foreach ($comments as $comment) {
     ?>
-    <div>
-        <h2><?= htmlspecialchars($article->getTitle()); ?></h2>
-        <p><?= htmlspecialchars($article->getContent()); ?></p>
-        <p><?= htmlspecialchars($article->getAuthor()); ?></p>
-        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt()); ?></p>
-    </div>
-    <br>
-
-    <a href="../public/index.php">Retour à l'accueil</a>
-
-    <div>
-        <h3>Commentaires</h3>
-
-        <?php
-        foreach ($comments as $comment) {
-        ?>
-        <h4><?php echo htmlspecialchars($comment->getPseudo()) ?></h4>
-        <p><?php echo htmlspecialchars($comment->getContent()) ?></p>
-        <p>Posté le <?php echo htmlspecialchars($comment->getCreatedAt()) ?></p>
-        <?php
-        }
-        ?>
-    </div>
-
-</body>
-
-</html>
+    <h4><?php echo htmlspecialchars($comment->getPseudo()) ?></h4>
+    <p><?php echo htmlspecialchars($comment->getContent()) ?></p>
+    <p>Posté le <?php echo htmlspecialchars($comment->getCreatedAt()) ?></p>
+    <?php
+    }
+    ?>
+</div>
