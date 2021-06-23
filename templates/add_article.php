45aa877936cf3f8966f9f68ac04170d4b336b104
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-06-23 19:31:55 +0200

    Refactorisation firmulaire

--- templates/add_article.php
+++ templates/add_article.php
@@ -1,18 +1,6 @@
 <?php $this->title = 'Nouvel article'; ?>
 
 <div>
-    <form method="POST" action="../public/index.php?route=addArticle">
-
-        <label for="title">Titre</label><br>
-        <input type="text" id='title' name='title'><br>
-
-        <label for="content">Titre</label><br>
-        <textarea name="content" id="content"></textarea><br>
-
-        <label for="author">Auteur</label><br>
-        <input type="text" id="author" name='author'><br>
-
-        <input type="submit" value='Envoyer' id='submit' name="submit">
-    </form>
+    <?php include('form_article.php') ?>
     <a href="../public/index.php">Retour à l'accueil</a>
 </div>
--- templates/edit_article.php
+++ templates/edit_article.php
@@ -1,19 +1,6 @@
 <?php $this->title = 'Modifier article' ?>
 
 <div>
-    <form method="POST" action="../public/index.php?route=editArticle&articleId=<?php echo htmlspecialchars($article->getId()) ?>">
-
-        <label for="title">Titre</label><br>
-        <input type="text" id="title" name="title" value="<?php echo htmlspecialchars($article->getTitle()) ?>"><br>
-
-        <label for="content">Contenu</label><br>
-        <textarea name="content" id="content"><?php echo htmlspecialchars($article->getContent()) ?></textarea><br>
-
-        <label for="author">Auteur</label><br>
-        <input type="text" id="author" name="author" value="<?php echo htmlspecialchars($article->getAuthor()) ?>"><br>
-
-        <input type="submit" value="Mettre à jour" id="submit" name="submit">
-    </form>
-
+    <?php include('form_article.php') ?>
     <a href="../public/index.php">Retour à l'accueil</a>
 </div>
