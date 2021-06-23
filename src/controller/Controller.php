2b2e1a2959ea08281b60e9ed2d3c6acfcf5fde64
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-06-23 20:55:23 +0200

    Contraintes et Validation

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -3,6 +3,7 @@
 namespace App\src\controller;
 
 use App\config\Request;
+use App\src\constraint\Validation;
 use App\src\DAO\ArticleDAO;
 use App\src\DAO\CommentDAO;
 use App\src\model\View;
@@ -15,12 +16,13 @@ abstract class Controller
     protected $get;
     protected $post;
     protected $session;
-
+    protected $validation;
     public function __construct()
     {
         $this->articleDAO = new ArticleDAO();
         $this->commentDAO = new CommentDAO();
         $this->view = new View();
+        $this->validation = new Validation();
         $request = new Request();
         $this->get = $request->getGet();
         $this->post = $request->getPost();
--- templates/form_article.php
+++ templates/form_article.php
@@ -1,17 +1,24 @@
 <?php
 $route = isset($article) && $article->getId() ? 'editArticle&articleId=' . $article->getId() : 'addArticle';
 $submit = $route === "addArticle" ? 'Envoyer' : "Mettre à jour";
-$title = isset($article) && $article->getId() ? htmlspecialchars($article->getTitle()) : "";
-$content = isset($article) && $article->getId() ? htmlspecialchars($article->getContent()) : "";
-$author = isset($article) && $article->getId() ? htmlspecialchars($article->getAuthor()) : "";
+$title = isset($post) ? htmlspecialchars($post->get('title')) : '';
+$content = isset($post) ? htmlspecialchars($post->get('content')) : '';
+$author = isset($post) ? htmlspecialchars($post->get('author')) : '';
 ?>
 
 <form method="post" action="../public/index.php?route=<?php echo $route ?>">
+
     <label for="title">Titre</label><br>
     <input type="text" id="title" name="title" value="<?php echo $title ?>"><br>
+    <?php echo isset($errors['title']) ? $errors['title'] : "" ?>
+
     <label for="content">Contenu</label><br>
     <textarea id="content" name="content"><?php echo $content ?></textarea><br>
+    <?php echo isset($errors['content']) ? $errors['content'] : "" ?>
+
     <label for="author">Auteur</label><br>
     <input type="text" id="author" name="author" value="<?php echo $author ?>"><br>
+    <?php echo isset($errors['author']) ? $errors['author'] : "" ?>
+
     <input type="submit" value="<?php echo $submit ?>" id="submit" name="submit">
 </form>
